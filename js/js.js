9fd55a37b4e84bc93bc7d67f73d62aaeccf07977
Hrolfr <sabrina.sales163@gmail.com>
2021-01-18 20:18:24 -0300

    anima2 e 3 add,linkados no js e prefixos dos browsers

--- js/js.js
+++ js/js.js
@@ -3,9 +3,9 @@ function start() { // Inicio da função start()
 	$("#inicio").hide(); //Oculta.Só posso usar esses comandos por causa do jquery.Obs: use jquery.
 	
 	$("#fundoGame").append("<div id='jogador' class='anima1'></div>");
-	$("#fundoGame").append("<div id='inimigo1'></div>");
+	$("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
 	$("#fundoGame").append("<div id='inimigo2'></div>");
-	$("#fundoGame").append("<div id='amigo'></div>");
+	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
     //Esse código a cima cria as divs adicionando um "caminho"
 } // Fim da função start
 
