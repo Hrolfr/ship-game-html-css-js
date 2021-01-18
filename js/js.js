7239a86932fe831c1906c57d364e8d6d03090d20
Hrolfr <sabrina.sales163@gmail.com>
2021-01-18 19:55:53 -0300

    chamei a função start,código do js

--- js/js.js
+++ js/js.js
@@ -1,11 +1,11 @@
 function start() { // Inicio da função start()
 
-	$("#inicio").hide();
+	$("#inicio").hide(); //Oculta.Só posso usar esses comandos por causa do jquery.Obs: use jquery.
 	
 	$("#fundoGame").append("<div id='jogador'></div>");
 	$("#fundoGame").append("<div id='inimigo1'></div>");
 	$("#fundoGame").append("<div id='inimigo2'></div>");
 	$("#fundoGame").append("<div id='amigo'></div>");
-
+    //Esse código a cima cria as divs adicionando um "caminho"
 } // Fim da função start
 
