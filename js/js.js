219d24bc6e208b975e0115ffbed3d42527d9ff88
Hrolfr <sabrina.sales163@gmail.com>
2021-01-18 20:05:47 -0300

    add o anima1 e o primeiro keyframes no js além de linkar o anima1 no js

--- js/js.js
+++ js/js.js
@@ -2,7 +2,7 @@ function start() { // Inicio da função start()
 
 	$("#inicio").hide(); //Oculta.Só posso usar esses comandos por causa do jquery.Obs: use jquery.
 	
-	$("#fundoGame").append("<div id='jogador'></div>");
+	$("#fundoGame").append("<div id='jogador' class='anima1'></div>");
 	$("#fundoGame").append("<div id='inimigo1'></div>");
 	$("#fundoGame").append("<div id='inimigo2'></div>");
 	$("#fundoGame").append("<div id='amigo'></div>");
