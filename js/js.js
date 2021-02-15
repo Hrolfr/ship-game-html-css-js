29996fe74a8d34dc9f052fb45ce1457fd04831d3
Hrolfr <sabrina.sales163@gmail.com>
2021-02-15 20:20:07 -0300

    func disparo

--- js/js.js
+++ js/js.js
@@ -76,6 +76,7 @@ function movejogador() {
 	if (jogo.pressionou[TECLA.D]) {
 		
 		//Chama função Disparo	
+		disparo();
 	}
 
 	} // fim da função movejogador()
@@ -117,6 +118,40 @@ function moveamigo() {
 		}
 
 } // fim da função moveamigo()
+
+function disparo() {
+	
+	if (podeAtirar==true) {
+		
+	podeAtirar=false;
+	
+	topo = parseInt($("#jogador").css("top"))
+	posicaoX= parseInt($("#jogador").css("left"))
+	tiroX = posicaoX + 190;
+	topoTiro=topo+37;
+	$("#fundoGame").append("<div id='disparo'></div");
+	$("#disparo").css("top",topoTiro);
+	$("#disparo").css("left",tiroX);
+	
+	var tempoDisparo=window.setInterval(executaDisparo, 30);
+	
+	} //Fecha podeAtirar
+ 
+   	    function executaDisparo() {
+	    posicaoX = parseInt($("#disparo").css("left"));
+	    $("#disparo").css("left",posicaoX+15); 
+
+        		if (posicaoX>900) {
+						
+			window.clearInterval(tempoDisparo);
+			tempoDisparo=null;
+			$("#disparo").remove();
+			podeAtirar=true;
+					
+                   }
+	} // Fecha executaDisparo()
+} // Fecha disparo()
+
 
 } // Fim da função loop()
 
