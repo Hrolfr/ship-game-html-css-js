c96b840efd6db27e27960fde417a37bc0def877f
Hrolfr <sabrina.sales163@gmail.com>
2021-04-19 14:28:32 -0300

    div placar e css

--- js/js.js
+++ js/js.js
@@ -6,7 +6,8 @@ function start() { // Inicio da função start()
 	$("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
 	$("#fundoGame").append("<div id='inimigo2'></div>");
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
-    //Esse código a cima cria as divs adicionando um "caminho"
+	$("#fundoGame").append("<div id='placar'></div>"); 
+	//Esse código a cima cria as divs adicionando um "caminho"
 
 
 //Principais variáveis do jogo
